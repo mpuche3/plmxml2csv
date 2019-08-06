219cd900e72b866daacbeb95f1881240adefaf08
Miguel <22322402+mpuche3@users.noreply.github.com>
2019-08-06 15:10:27 +0100

    Add files via upload

---
+++ js/script_01.js
@@ -0,0 +1,39 @@
+document.querySelector('#inputText').addEventListener('drop', e => {
+    e.preventDefault();
+    document.querySelector('#inputText').classList.remove("highlightedLine");
+    if (e.dataTransfer.items) {
+        let f = e.dataTransfer.files[0];
+        let r = new FileReader();
+        r.onload = e => inputText.value = e.target.result;
+        r.readAsText(f);
+    } else {
+        for (var i = 0; i < e.dataTransfer.files.length; i++) {
+            console.log('... file[' + i + '].name = ' + e.dataTransfer.files[i].name);
+            fichero = file;
+        }
+    }
+}, false);
+
+document.querySelector('#inputText').addEventListener('dragenter', e => {
+    e.preventDefault();
+    console.log('File(s) entered drop zone');
+    document.querySelector("#inputText").classList.add("highlightedLine");
+}, false);
+
+document.querySelector('#inputText').addEventListener('dragleave', e => {
+    e.preventDefault();
+    console.log('File(s) left drop zone');
+    document.querySelector("#inputText").classList.remove("highlightedLine");
+}, false);
+
+document.querySelector('.kc_fab_main_btn').addEventListener('click', e => {
+    const bool = document.querySelector('.message-container').classList.toggle("message-container-on");
+    setTimeout(_=>document.querySelector('.message').classList.add("message-on"), 400);
+    document.querySelector('.kc_fab_main_btn').classList.toggle("kc_fab_main_btn-message-on");
+}, false);
+
+document.querySelector('#close').addEventListener('click', e => {
+    const bool = document.querySelector('.message-container').classList.toggle("message-container-on");
+    document.querySelector('.message').classList.remove("message-on");
+    setTimeout(_=>document.querySelector('.kc_fab_main_btn').classList.toggle("kc_fab_main_btn-message-on"), 400);
+}, false);
---
+++ js/script_02.js
@@ -0,0 +1,95 @@
+function addQty(arr, id) {
+    const arrStr = arr.map(part=>part[id]);
+    const setStr = [...new Set(arrStr)];
+    let children = [];
+    for (const str of setStr) {
+        let partUnique = {};
+        let qty = 0;
+        for (const part of arr) {
+            if (str === part[id]) {
+                qty += 1;
+                partUnique = part;
+            }
+        }
+        children.push({
+            qty: qty,
+            part: partUnique
+        });
+    }
+    return children;
+}
+
+document.querySelector("#button").onclick = event=>{
+    const xml = document.querySelector('#inputText').value;
+    const input = xml2json(parseXml(xml));
+    const productInstance = input.PLMXML.ProductDef.InstanceGraph.ProductInstance
+    const productRevisionView = input.PLMXML.ProductDef.InstanceGraph.ProductRevisionView
+
+    prodInsObj = {};
+    prodIns = productInstance.map(x=>{
+        return {
+            id: x["@id"],
+            name: x["@name"],
+            partRef: x["@partRef"]
+        }
+    }
+    ).map(x=>{
+        prodInsObj[x.id] = x;
+        return x;
+    }
+    );
+
+    prodRevViewObj = {};
+    prodRevView = productRevisionView.map(x=>{
+        return {
+            id: x["@id"],
+            name: x["@name"],
+            children: x["@instanceRefs"]
+        }
+    }
+    ).map(x=>{
+        if (x.children !== undefined)
+            x.children = x.children.trim().split(" ");
+        return x;
+    }
+    ).map(x=>{
+        if (x.children !== undefined)
+            x.children = x.children.map(id=>prodInsObj[id]);
+        return x;
+    }
+    ).map(x=>{
+        prodRevViewObj["#" + x.id] = x;
+        return x;
+    }
+    ).map(x=>{
+        if (x.children !== undefined)
+            x.children = x.children.map(obj=>prodRevViewObj[obj.partRef]);
+        return x;
+    }
+    ).map(x=>{
+        if (x.children !== undefined)
+            x.children = x.children.sort((a,b)=>a.name.localeCompare(b.name));
+        return x;
+    }
+    ).map(x=>{
+        if (x.children !== undefined)
+            x.children = addQty(x.children, "id");
+        return x;
+    }
+    ).sort((a,b)=>a.name.localeCompare(b.name));
+
+    console.log(prodRevView);
+
+    let strOutput = '';
+    for (const part of prodRevView) {
+        if (part.children !== undefined) {
+            strOutput += '\n'
+            strOutput += part.name + '\n'
+            for (child of part.children) {
+                strOutput += ">>> " + child.part.name + '|' + child.qty + '\n'
+            }
+        }
+    }
+
+    document.querySelector("#outputText").value = strOutput;
+}
