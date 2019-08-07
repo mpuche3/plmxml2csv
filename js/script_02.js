251a6e77f0bed23cec39989270b2508ed2627225
MIGUEL PUCHE ALONSO <miguel@MIGUELs-MacBook-Pro.local>
2019-08-07 14:49:45 +0100

    first commit

--- js/script_02.js
+++ js/script_02.js
@@ -1,5 +1,5 @@
 function addQty(arr, id) {
-    const arrStr = arr.map(part=>part[id]);
+    const arrStr = arr.map(part => part[id]);
     const setStr = [...new Set(arrStr)];
     let children = [];
     for (const str of setStr) {
@@ -19,64 +19,55 @@ function addQty(arr, id) {
     return children;
 }
 
-document.querySelector("#button").onclick = event=>{
-    const xml = document.querySelector('#inputText').value;
+
+function run() {
     const input = xml2json(parseXml(xml));
     const productInstance = input.PLMXML.ProductDef.InstanceGraph.ProductInstance
     const productRevisionView = input.PLMXML.ProductDef.InstanceGraph.ProductRevisionView
 
     prodInsObj = {};
-    prodIns = productInstance.map(x=>{
+    prodIns = productInstance.map(x => {
         return {
             id: x["@id"],
             name: x["@name"],
             partRef: x["@partRef"]
         }
-    }
-    ).map(x=>{
+    }).map(x => {
         prodInsObj[x.id] = x;
         return x;
-    }
-    );
+    });
 
     prodRevViewObj = {};
-    prodRevView = productRevisionView.map(x=>{
+    prodRevView = productRevisionView.map(x => {
         return {
             id: x["@id"],
             name: x["@name"],
             children: x["@instanceRefs"]
         }
-    }
-    ).map(x=>{
+    }).map(x => {
         if (x.children !== undefined)
             x.children = x.children.trim().split(" ");
         return x;
-    }
-    ).map(x=>{
+    }).map(x => {
         if (x.children !== undefined)
-            x.children = x.children.map(id=>prodInsObj[id]);
+            x.children = x.children.map(id => prodInsObj[id]);
         return x;
-    }
-    ).map(x=>{
+    }).map(x => {
         prodRevViewObj["#" + x.id] = x;
         return x;
-    }
-    ).map(x=>{
+    }).map(x => {
         if (x.children !== undefined)
-            x.children = x.children.map(obj=>prodRevViewObj[obj.partRef]);
+            x.children = x.children.map(obj => prodRevViewObj[obj.partRef]);
         return x;
-    }
-    ).map(x=>{
+    }).map(x => {
         if (x.children !== undefined)
-            x.children = x.children.sort((a,b)=>a.name.localeCompare(b.name));
+            x.children = x.children.sort((a, b) => a.name.localeCompare(b.name));
         return x;
-    }
-    ).map(x=>{
+    }).map(x => {
         if (x.children !== undefined)
             x.children = addQty(x.children, "id");
         return x;
-    }
-    ).sort((a,b)=>a.name.localeCompare(b.name));
+    }).sort((a, b) => a.name.localeCompare(b.name));
 
     console.log(prodRevView);
 
@@ -90,6 +81,15 @@ document.querySelector("#button").onclick = event=>{
             }
         }
     }
+    return strOutput;
+}
 
+
+document.querySelector("#button").onclick = event => {
+
+    const xml = document.querySelector('#inputText').value;
+
+
+    strOutput = run(xml);
     document.querySelector("#outputText").value = strOutput;
 }
